a300f82da04d2be4fe36cec3d1453990d9206d85
Muhammad Usman <80684477+M-Usman143@users.noreply.github.com>
2025-03-19 12:33:26 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:isaar/Common/InternetChecker.dart';
 import 'Common/FavoriteProvider.dart';
 import 'Pages/SplashScreen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensures proper binding
-  await Firebase.initializeApp(); // Initializes Firebase
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-  runApp(  MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => FavoriteProvider()),
-    ],
-    child: MyApp(),
-  ),);
+  runApp(const MyAppWrapper());
+}
+
+class MyAppWrapper extends StatelessWidget {
+  const MyAppWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => FavoriteProvider()),
+      ],
+      child: const InternetChecker(
+        child: MyApp(),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -28,7 +40,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      home: SplashScreen()
     );
   }
 }
